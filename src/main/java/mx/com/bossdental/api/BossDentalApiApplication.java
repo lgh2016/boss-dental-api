b621d6cf1940472bc94f51aeb7150554c4f37035
LuisGH <garduno2013luis@gmail.com>
2026-05-14 13:30:34 -0600

    initial backend setup with docker and postgres

---
+++ src/main/java/mx/com/bossdental/api/BossDentalApiApplication.java
@@ -0,0 +1,13 @@
+package mx.com.bossdental.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BossDentalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BossDentalApiApplication.class, args);
+	}
+
+}

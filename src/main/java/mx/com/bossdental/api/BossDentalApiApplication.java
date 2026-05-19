92e03444ae7d62e487c7f7b8e33fcb835102dce6
LuisGH <garduno2013luis@gmail.com>
2026-05-18 22:03:09 -0600

    Ajuste de tiempo

--- src/main/java/mx/com/bossdental/api/BossDentalApiApplication.java
+++ src/main/java/mx/com/bossdental/api/BossDentalApiApplication.java
@@ -1,11 +1,19 @@
 package mx.com.bossdental.api;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class BossDentalApiApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Mexico_City"));
+	}
 
 	public static void main(String[] args) {
 		//System.out.println("### test: " + new BCryptPasswordEncoder().encode("admin123"));
